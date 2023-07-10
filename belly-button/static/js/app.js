2e695cdac7830082e9d9252a0dc03689bf3a98b4
Mario Martinez <=>
2023-07-10 17:22:38 -0500

    Moving slowly

--- belly-button/static/js/app.js
+++ belly-button/static/js/app.js
@@ -1,11 +1,31 @@
 // Using D3 to read in samples.json
-const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
-let data = d3.json(url);
+let sampleData = d3.json('samples.json').then((data) => {console.log(data)});
+// data.samples[0].sample_values
 
-let barChart = {
-    x:
-    y: ,
-    text: ,
-    type: 'bar',
-    orientation: 'h'
-};
+var data.sort((a, b) => b.samples[0].sample_values - a.samples[0].sample_values) 
+
+// var sample_values =
+// var otu_ids =
+// var otu_lables =
+
+// let barChart = {
+//     x: 100,
+//     y: 50,
+//     text: 'Bar Chart',
+//     type: 'bar',
+//     orientation: 'h'
+// };
+
+// traceData = [barChart];
+
+// let layout = {
+//     title: 'My First Chart',
+//     margin: {
+//         l: 100,
+//         r: 100,
+//         t: 100,
+//         b: 100
+//     }
+// };
+
+// Plotly.newPlot('plot', traceData, layout);
