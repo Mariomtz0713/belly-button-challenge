fb84a5304eb79257eeaf890a3a591ab60ea9cc59
Mario Martinez <mariomtz2023@gmail.com>
2023-07-12 17:33:17 -0500

    I FIGURED OUT THE LINE BREAKS

--- belly-button/static/js/app.js
+++ belly-button/static/js/app.js
@@ -1,47 +1,45 @@
 // Default plot
 function init() {
 
-d3.json('samples.json').then((data) => {console.log(data);
+d3.json('samples.json').then((data) => {
+
+console.log(data);
 
 var allIds = data.metadata;
 var firstIdInfo = data.metadata[0];
 var queryID = data.names[0];
 var queryData = data.samples.filter(person => person.id == queryID)[0];
-d3.select('#sample-metadata').text(
-    `id: ${firstIdInfo.id}
-    ethinicity: ${firstIdInfo.ethnicity}
-    gender: ${firstIdInfo.gender}
-    age: ${firstIdInfo.age}
-    location: ${firstIdInfo.location}
-    bbtype: ${firstIdInfo.bbtype}
+
+// Displaying first demographic info
+d3.select('#sample-metadata').html(
+    `id: ${firstIdInfo.id} <br>
+    ethinicity: ${firstIdInfo.ethnicity} <br>
+    gender: ${firstIdInfo.gender} <br>
+    age: ${firstIdInfo.age} <br>
+    location: ${firstIdInfo.location} <br>
+    bbtype: ${firstIdInfo.bbtype} <br>
     wfreq: ${firstIdInfo.wfreq}`
     )
-console.log(firstIdInfo)
+
 
 // Arrays of data values for charts
 var sampleValues = queryData.sample_values;
 var otuIds = queryData.otu_ids;
 var otuLabels = queryData.otu_labels;
 
-
 // Top 10 data values
-var sortedSampleValues = sampleValues.sort((a, b) => b - a);
-var top10SampleValues = sortedSampleValues.slice(0, 10).reverse();
-
+var top10SampleValues = sampleValues.slice(0, 10).reverse();
 var top10OtuIds = otuIds.slice(0, 10).reverse();
-
-var sortedOtuLabels = otuLabels.sort((a,b) => b - a);
-var top10OtuLabes = sortedOtuLabels.slice(0, 10).reverse();
+var top10OtuLabes = otuLabels.slice(0, 10).reverse();
 
 // Creating Bar Chart
 let barChart = [{
     x: top10SampleValues,
-    y: top10OtuIds.map(x => 'OTU ' + x),
+    y: top10OtuIds.map(x => `OTU  ${x}`),
     text: top10OtuLabes,
     type: 'bar',
     orientation: 'h'
 }];
-
 Plotly.newPlot('bar', barChart);
 
 // Creating Bubble Chart
@@ -51,10 +49,9 @@ let bubbleChart = [{
     text: otuLabels,
     mode: 'markers',
     marker: {
-        color: otuIds,
-        size: sampleValues
+        size: sampleValues,
+        color: otuIds
     }
-
 }];
 
 let layout = {
@@ -64,17 +61,27 @@ let layout = {
         title: 'OTU ID'
     }
 };
-
 Plotly.newPlot('bubble', bubbleChart, layout)
+
 
     })
 
 };
 
 
+
 // Updated plot
 function plotupdated () {
-d3.json('samples.json').then((data) => (console.log(data)))
+d3.json('samples.json').then((data) => {
+
+console.log(data) 
+
+
+
+
+
+
+    })
 };
 
 
