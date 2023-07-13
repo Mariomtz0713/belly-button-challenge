7e7d4a0e5f61633b8309d1e76ce700a8d971ec82
Mario Martinez <mariomtz2023@gmail.com>
2023-07-13 01:29:21 -0500

    Belly-button complete

--- belly-button/static/js/app.js
+++ belly-button/static/js/app.js
@@ -1,87 +1,89 @@
-// Default plot
-function init() {
+// Initial page load
+function init () {
 
-d3.json('samples.json').then((data) => {
+    d3.json('samples.json').then((data) => {
+    
+    console.log(data);
+    
+    // Creating dropdown id menu
+    let allIds = data.names;
+    allIds.forEach((idNumber) => {d3.select('#selDataset').append('option').text(idNumber)});
 
-console.log(data);
-
-var allIds = data.metadata;
-var firstIdInfo = data.metadata[0];
-var queryID = data.names[0];
-var queryData = data.samples.filter(person => person.id == queryID)[0];
-
-// Displaying first demographic info
-d3.select('#sample-metadata').html(
-    `id: ${firstIdInfo.id} <br>
-    ethinicity: ${firstIdInfo.ethnicity} <br>
-    gender: ${firstIdInfo.gender} <br>
-    age: ${firstIdInfo.age} <br>
-    location: ${firstIdInfo.location} <br>
-    bbtype: ${firstIdInfo.bbtype} <br>
-    wfreq: ${firstIdInfo.wfreq}`
-    )
-
-
-// Arrays of data values for charts
-var sampleValues = queryData.sample_values;
-var otuIds = queryData.otu_ids;
-var otuLabels = queryData.otu_labels;
-
-// Top 10 data values
-var top10SampleValues = sampleValues.slice(0, 10).reverse();
-var top10OtuIds = otuIds.slice(0, 10).reverse();
-var top10OtuLabes = otuLabels.slice(0, 10).reverse();
-
-// Creating Bar Chart
-let barChart = [{
-    x: top10SampleValues,
-    y: top10OtuIds.map(x => `OTU  ${x}`),
-    text: top10OtuLabes,
-    type: 'bar',
-    orientation: 'h'
-}];
-Plotly.newPlot('bar', barChart);
-
-// Creating Bubble Chart
-let bubbleChart = [{
-    x: otuIds,
-    y: sampleValues,
-    text: otuLabels,
-    mode: 'markers',
-    marker: {
-        size: sampleValues,
-        color: otuIds
-    }
-}];
-
-let layout = {
-    height: 600,
-    width: 1000,
-    xaxis: {
-        title: 'OTU ID'
-    }
-};
-Plotly.newPlot('bubble', bubbleChart, layout)
-
-
+    // Sending info to charts function to create inital charts
+    charts(allIds[0])
+    
+    // End of block
     })
 
 };
 
 
+// Creating charts
+function charts(queryID) {
 
-// Updated plot
-function plotupdated () {
-d3.json('samples.json').then((data) => {
+    d3.json('samples.json').then((data) => {
 
-console.log(data) 
+    // Making queries
+    var queryData = data.samples.filter(person => person.id == queryID)[0];
+    var queryMetadata = data.metadata.filter(person => person.id == queryID)[0];
 
+    // Printing metadata to page
+    let metadataBox = d3.select('#sample-metadata').html("");
+    for (key in queryMetadata) {
+        metadataBox.append('h5').text(`${key}: ${queryMetadata[key]}`)
+    };
+    
+    // Arrays of data values for charts
+    var sampleValues = queryData.sample_values;
+    var otuIds = queryData.otu_ids;
+    var otuLabels = queryData.otu_labels;
 
+    // Top 10 data values
+    var top10SampleValues = sampleValues.slice(0, 10).reverse();
+    var top10OtuIds = otuIds.slice(0, 10).reverse();
+    var top10OtuLabes = otuLabels.slice(0, 10).reverse();
 
+    // Creating Bar Chart
+    let barChart = [{
+        x: top10SampleValues,
+        y: top10OtuIds.map(x => `OTU  ${x}`),
+        text: top10OtuLabes,
+        type: 'bar',
+        orientation: 'h'
+    }];
+    Plotly.newPlot('bar', barChart);
 
+    // Creating Bubble Chart
+    let bubbleChart = [{
+        x: otuIds,
+        y: sampleValues,
+        text: otuLabels,
+        mode: 'markers',
+        marker: {
+            size: sampleValues,
+            color: otuIds,
+            colorscale: 'Bluered'
+        }
+    }];
 
+    let layout = {
+        height: 600,
+        width: 1000,
+        xaxis: {
+            title: 'OTU ID'
+        }
+    };
+    Plotly.newPlot('bubble', bubbleChart, layout)
 
+    // End of block
     })
+
+};
+
+
+// Updating plot
+function optionChanged(id) {
+    charts(id)
 };
 
 
